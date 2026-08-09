86d4318204a5f6c4afaeca2bd2d5ba7ad28ebddf
Sergey-Simbirtsev <Seregasimba@yandex.ru>
2026-08-10 00:14:28 +0300

    Отформатировать новые файлы под codeStyle

--- src/test/java/analyticFunction/DetermineTypeUserTest.java
+++ src/test/java/analyticFunction/DetermineTypeUserTest.java
@@ -14,6 +14,7 @@ import java.time.LocalDateTime;
 import static java.time.Month.OCTOBER;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+
 public class DetermineTypeUserTest {
     private static DetermineTypeUser DetermineTypeUser;
     private static SleepSessions sleepSessions;
