874347c3a93c38f19f50e8697f43e8de75ad42d0
Sergey-Simbirtsev <Seregasimba@yandex.ru>
2026-08-10 00:42:15 +0300

    Добавить комментарии

--- src/main/java/ru/yandex/practicum/sleeptracker/SleepTrackerApp.java
+++ src/main/java/ru/yandex/practicum/sleeptracker/SleepTrackerApp.java
@@ -26,6 +26,7 @@ public class SleepTrackerApp {
             return;
         }
 
+        // Записыаем путь log-файла
         final String fileName = args[0];
 
         // Создаём объект загрузки
